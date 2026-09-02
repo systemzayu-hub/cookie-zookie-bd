77b8fd420fe6b389f90fd7433a2cfa086b9ea820
systemzayu-hub <systemzayu.hub@gmail.com>
2026-09-02 15:19:44 -0300

    base relativon p/ GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: { open: true, port: 5174 },
   build: {
     rollupOptions: {
